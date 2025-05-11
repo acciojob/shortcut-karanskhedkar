4dcae7f16cef23b2b9d533fdc6a4c6900f95737e
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-11 11:22:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function shortcut(s1, s2) {
-  // your code here
+  // your code 
+	if(s1.length && s2.length){
+		return s1[0]+s2[0];
+	}
+	return "";
 }
 
 // Do not change the code below.
+
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
